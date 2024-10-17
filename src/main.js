985017a0ab223e8b677d23b81f4bbf12052f0889
MyTruQs <raulfernandes.rm@gmail.com>
2024-10-17 09:44:42 -0300

    update project

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// CSS
+import "./styles/global.css";
+import "./styles/header.css";
+import "./styles/profile-area.css";
+import "./styles/fidelity-area.css";
+import "./styles/history-area.css";
+import "./styles/gift-area.css";
+
+// JS
+import "./modules/inputs-update.js";
+import "./modules/search-client.js";
